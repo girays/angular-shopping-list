3cab43cc250dffa3837595e03bcdedc137e0c678
Giray Simsek <giray.simsek@zurich.com>
2021-12-22 09:10:59 +0100

    Add shopping list feature.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,7 +8,8 @@ import {Recipe} from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test.', 'https://www.themediterraneandish.com/wp-content/uploads/2020/02/falafel-recipe-10.jpg')
+    new Recipe('A Test Recipe', 'This is simply a test.', 'https://image.shutterstock.com/image-photo/blank-vintage-recipe-cooking-book-600w-504504346.jpg'),
+    new Recipe('Indian food recipe', 'Indian food.', 'https://www.brit.co/media-library/this-veggie-filled-butter-chicken-curry-is-a-top-indian-food-recipe-on-our-list.jpg?id=21210913&width=600&quality=90')
   ];
 
   constructor() { }
